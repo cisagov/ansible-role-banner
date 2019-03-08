213b21022c6a1b5369804f5f71b47e5a3ce61e7c
Felddy <markf+github@geekpad.com>
2019-03-08 11:11:10 -0500

    Update failing tests.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,25 +4,27 @@ import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('pkg', [
-    'openssh-server'
-])
+@pytest.mark.parametrize("pkg", ["openssh-server"])
 def test_packages(host, pkg):
     package = host.package(pkg)
 
     assert package.is_installed
 
 
-@pytest.mark.parametrize('file,contents', [
-    ('/etc/motd', "|.   '|'   ..|'''.|     |     |''||''|  .|'''.|"),
-    # Yes, the two spaces after 'information' are really intentional.
-    # See files/issue.
-    ('/etc/issue', 'U.S. Government information  system'),
-    ('/etc/ssh/sshd_config', '^Banner /etc/issue'),
-])
+@pytest.mark.parametrize(
+    "file,contents",
+    [
+        ("/etc/motd", "CYBER + INFRASTRUCTURE"),
+        # Yes, the two spaces after 'information' are really intentional.
+        # See files/issue.
+        ("/etc/issue", "U.S. Government information  system"),
+        ("/etc/ssh/sshd_config", "^Banner /etc/issue"),
+    ],
+)
 def test_files_exist(host, file, contents):
     f = host.file(file)
 
